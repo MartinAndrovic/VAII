d6dba2baefb2a663ea0c7884ce4b08276ca36de7
MartinAndrovic <martin.androvic@gmail.com>
2021-02-10 21:44:28 +0100

    21.44

--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -5,26 +5,11 @@
         <img src="/storage/{{$post->obrazok}}" alt="{{$post->nazov}}">
         <h1 class="postName">{{$post->nazov}}</h1>
         <p class="small catText">Kategória: {{$post->category->nazov}}</p>
-
+        <p class="postUvod">{{$post->uvod}}</p>
         <p>{{$post->text}}</p>
 
-        <h2>Komentare</h2>
-        @auth
-            <h3>Tu napiste svoj komentar</h3>
-            <form action="/komentar" method="POST">
-                @csrf
-                <div class="input-wrapper ">
-                    <label for="kategoria"> Komentar </label>
-                    <input type="text" id="text" name="text" placeholder="Komentar">
-                    @error('text')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                    <input type="hidden" value="{{$post->id}}" name="post_id">
-                    <button type="submit">Komentovat</button>
+        <h2 class="comName">Komentare</h2>
 
-                </div>
-            </form>
-        @endauth
         <div class="commentare">
         @forelse($post->comments as $comment)
             <div class="comment">
@@ -35,6 +20,24 @@
             <h2>Ziadne komentare</h2>
         @endforelse
         </div>
+
+    @auth
+        <!--   <h3>Tu napiste svoj komentar</h3> -->
+            <form action="/komentar" method="POST">
+                @csrf
+                <div class="input-wrapper ">
+
+                    <textarea type="text" id="text" name="text" placeholder="Komentar"> </textarea>
+
+                    @error('text')
+                    <div class="alert alert-danger">{{ $message }}</div>
+                    @enderror
+                    <input type="hidden" value="{{$post->id}}" name="post_id">
+                    <button  class="submit" type="submit">Komentovat</button>
+
+                </div>
+            </form>
+        @endauth
 
 
     </div>
--- blog/resources/views/posts/showUser.blade.php
+++ blog/resources/views/posts/showUser.blade.php
@@ -2,21 +2,32 @@
 
 @section('content')
     <div class="post-show">
-        <a href="/posts/{{$post->id}}/edit">Edit</a>
-        <form id="postDelete" method="POST">
-            @csrf
-            @method('DELETE')
-            <input type="hidden" name="post_id" value="{{$post->id}}" id="post_id_delete">
-            <button type="submit">Vymazat</button>
-        </form>
+
         <img src="/storage/{{$post->obrazok}}" alt="{{$post->nazov}}">
-        <h1>{{$post->nazov}}</h1>
+
+
+
+        <h1 class="postName">{{$post->nazov}}</h1>
+        <p class="postUvod">{{$post->uvod}}</p>
         <p class="catText">Categoria: {{$post->category->nazov}}</p>
         <p class="catText">{{$post->text}}</p>
 
-        <h2>Komentare</h2>
+        <h2 class="comName">Komentare</h2>
+
+
+        @forelse($post->comments as $comment)
+
+            <div class="comment">
+                <p>{{$comment->text}}</p>
+                <p>Napisal : {{$comment->user->name}}</p>
+            </div>
+
+        @empty
+            <h2>Ziadne komentare</h2>
+        @endforelse
+
         @auth
-            <h3>Komentovat vlastny prispevok</h3>
+          <!--  <h3>Komentovat vlastny prispevok</h3> -->
             <form action="/komentar" method="POST">
                 @csrf
                 <div class="input-wrapper ">
@@ -32,18 +43,16 @@
             </form>
         @endauth
 
-        @forelse($post->comments as $comment)
 
-            <div class="comment">
-                <p>{{$comment->text}}</p>
-                <p>Napisal : {{$comment->user->name}}</p>
-            </div>
-
-        @empty
-            <h2>Ziadne komentare</h2>
-        @endforelse
-
-
+        <div class="edit">
+        <a href="/posts/{{$post->id}}/edit">Edit</a>
+        <form id="postDelete" method="POST">
+            @csrf
+            @method('DELETE')
+            <input type="hidden" name="post_id" value="{{$post->id}}" id="post_id_delete">
+            <button type="submit">Vymazat</button>
+        </form>
+        </div>
 
     </div>
 @endsection
